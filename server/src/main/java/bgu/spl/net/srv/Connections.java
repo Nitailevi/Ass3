52f78d516a030ae433353ba200cd0103338c42e5
Noa Regev <noare1304@gmail.com>
2025-01-18 22:09:02 +0000

    Fix: Implement connections and protocol

--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -1,12 +1,19 @@
 package bgu.spl.net.srv;
 
 import java.io.IOException;
+import java.util.function.Function;
 
 public interface Connections<T> {
 
     boolean send(int connectionId, T msg);
 
-    void send(String channel, T msg);
+    void send(String channel, Function<String, T> messageGenerator);
 
     void disconnect(int connectionId);
+
+
+    void subscribe(String channel, int connectionId, String subscriberId);
+    void unsubscribe(String subscriberId);
+    String authenticate(String login, String passcode, int connectionId);
+
 }
