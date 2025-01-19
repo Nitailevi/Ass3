12e7877a11f0577f05aa3ea7cb80ad9a6a926679
Noa Regev <noare1304@gmail.com>
2025-01-19 13:38:04 +0000

    add extends messaging protocol

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,41 @@
 package bgu.spl.net.impl.stomp;
+import bgu.spl.net.srv.Server;
+import bgu.spl.net.impl.stomp.StompMessagingProtocolImpl;
 
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
+        if (args.length < 2) {
+            System.out.println("Incorrect number of arguments");
+            return;
+        }
+
+        int port;
+        try {
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number: " + args[0]);
+            return;
+        }
+
+        String serverType = args[1].toLowerCase(); // Make sure the server type is case-insensitive
+
+        if (serverType.equals("tpc")) {
+            Server.<String>threadPerClient(
+                port,
+                StompMessagingProtocolImpl::new, // Correct Supplier for MessagingProtocol
+                StompEncoderDecoder::new // Correct Supplier for MessageEncoderDecoder
+            ).serve();
+        } else if (serverType.equals("reactor")) {
+            Server.<String>reactor(
+                Runtime.getRuntime().availableProcessors(), // Number of threads available
+                port,
+                StompMessagingProtocolImpl::new, // Correct Supplier for MessagingProtocol
+                StompEncoderDecoder::new // Correct Supplier for MessageEncoderDecoder
+            ).serve();
+        } else {
+            System.out.println("Unknown server type: " + serverType);
+            System.out.println("Valid options are 'reactor' or 'tpc'.");
+        }
     }
 }
